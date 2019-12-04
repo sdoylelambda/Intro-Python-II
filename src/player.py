77d60faa3316615eb580cc61aea4cb38569b790b
Sean Doyle <sdoylelambda@gmail.com>
2019-12-04 11:50:55 -0500

    Player class created with methods for location, attack and defence.

--- src/player.py
+++ src/player.py
@@ -1,2 +1,31 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init(room, health, damage):
+        self.room = room
+        self.health = health
+        self.damage = damage
+
+        def location(self, room):
+            if room == 'outside':
+                print(room.Room)
+            elif room == 'foyer':
+                print(room.Room)
+            elif room == 'overlook':
+                print(room.Room)
+            elif room == 'narrow':
+                print(room.Room)
+            elif room == 'treasure':
+                print(room.Room)
+            else:
+                print('You are lost')
+
+        def defence(self, enemy):
+            if self > 0 and enemy < 25:
+                health += enemy
+
+        def attack(self, enemy):
+            if enemy > 0 and damage < 25:
+                damage += enemy

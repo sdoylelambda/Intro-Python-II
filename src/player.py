f0c8af99cabec893da544367fc061e8a1782a75c
Sean Doyle <sdoylelambda@gmail.com>
2019-12-04 11:55:14 -0500

    Location methods moved to Room.py.

--- src/player.py
+++ src/player.py
@@ -4,23 +4,8 @@
 
 class Player:
     def __init(room, health, damage):
-        self.room = room
         self.health = health
         self.damage = damage
-
-        def location(self, room):
-            if room == 'outside':
-                print(room.Room)
-            elif room == 'foyer':
-                print(room.Room)
-            elif room == 'overlook':
-                print(room.Room)
-            elif room == 'narrow':
-                print(room.Room)
-            elif room == 'treasure':
-                print(room.Room)
-            else:
-                print('You are lost')
 
         def defence(self, enemy):
             if self > 0 and enemy < 25:
--- src/room.py
+++ src/room.py
@@ -1,2 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init(location):
+        self.room = room
+
+        def location(self, room):
+            if room == 'outside':
+                print(room.Room)
+            elif room == 'foyer':
+                print(room.Room)
+            elif room == 'overlook':
+                print(room.Room)
+            elif room == 'narrow':
+                print(room.Room)
+            elif room == 'treasure':
+                print(room.Room)
+            else:
+                print('You are lost')
